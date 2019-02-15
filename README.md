fc47c870a00345a237a67077c957e7ef4bdf72b8
gkeglevich <griffinkeglevich@gmail.com>
2019-02-15 17:47:12 -0500

    Modified everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# personal-website
-The source code to my personal website
---
+++ precache-manifest.ac03d3050e14a81a7ae2e37f245ef483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "59e8bdea2c6ad73e202f",
+    "url": "/static/js/main.59e8bdea.chunk.js"
+  },
+  {
+    "revision": "0cba906062e39f97fbb8",
+    "url": "/static/js/1.0cba9060.chunk.js"
+  },
+  {
+    "revision": "59e8bdea2c6ad73e202f",
+    "url": "/static/css/main.bf121ae8.chunk.css"
+  },
+  {
+    "revision": "0cba906062e39f97fbb8",
+    "url": "/static/css/1.8b5cd664.chunk.css"
+  },
+  {
+    "revision": "43c4efa14a14ef30a5944931e5fdf894",
+    "url": "/index.html"
+  }
+];
