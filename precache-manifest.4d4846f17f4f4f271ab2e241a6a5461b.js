7c1731dfccb18e1683aaff0aeec03c644c4f8409
gkeglevich <griffinkeglevich@gmail.com>
2019-02-27 19:01:54 -0500

    Updates

---
+++ precache-manifest.4d4846f17f4f4f271ab2e241a6a5461b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "835b91d40ef96d490361",
+    "url": "/static/js/main.835b91d4.chunk.js"
+  },
+  {
+    "revision": "2319093b72751ca7151e",
+    "url": "/static/js/1.2319093b.chunk.js"
+  },
+  {
+    "revision": "835b91d40ef96d490361",
+    "url": "/static/css/main.bd457614.chunk.css"
+  },
+  {
+    "revision": "2319093b72751ca7151e",
+    "url": "/static/css/1.2d17557f.chunk.css"
+  },
+  {
+    "revision": "8bfbd428e29f6b922f5b2b41a7e3f653",
+    "url": "/index.html"
+  }
+];
